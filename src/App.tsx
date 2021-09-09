9fd72bd2b81d92f98696a975a052515da22bfd16
Gabriel <silvagabrielbeltrame@gmail.com>
2021-09-08 23:22:56 -0300

    inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { GlobalStyle } from "./styles/global";
+import {Header} from "./components/Header";
+import {Dashboard} from "./components/Dashboard";
+import Modal from "react-modal";
+import {useState} from "react";
+import {NewTransactionModal} from "./components/NewTransactionModal";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+Modal.setAppElement('#root')
+
+export function App() {
+
+    const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+    function handleOpenNewTransactionModal() {
+        setIsNewTransactionModalOpen(true);
+    }
+
+    function handleCloseNewTransactionModal() {
+        setIsNewTransactionModalOpen(false);
+    }
+
+    return (
+        <>
+            <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+            <Dashboard />
+
+            <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal} />
+
+            <GlobalStyle />
+        </>
+    );
 }
-
-export default App;
---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,54 @@
+import Modal from "react-modal";
+import closeImg from '../../assets/close.svg'
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
+import { Container, TransactionTypeContainer } from "./styles";
+
+interface NewTransactionModalProps {
+    isOpen: boolean;
+    onRequestClose: () => void;
+}
+
+export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModalProps) {
+    return (
+        <Modal isOpen={isOpen} onRequestClose={onRequestClose} overlayClassName="react-modal-overlay" className="react-modal-content">
+
+            <button type="button" onClick={onRequestClose} className="react-modal-close">
+                <img src={closeImg} alt="Botão fechar" />
+            </button>
+
+            <Container>
+                <h2>Cadastrar transação</h2>
+
+                <input
+                    placeholder="Título"
+                />
+
+                <input
+                    type="number"
+                    placeholder="Valor"
+                />
+
+                <TransactionTypeContainer >
+                    <button type="button">
+                        <img src={incomeImg} alt="Entrada" />
+                        <span>Entrada</span>
+                    </button>
+                </TransactionTypeContainer>
+
+                <TransactionTypeContainer >
+                    <button type="button">
+                        <img src={outcomeImg} alt="saída" />
+                        <span>Saída</span>
+                    </button>
+                </TransactionTypeContainer>
+
+                <input
+                    placeholder="Categoria"
+                />
+
+                <button type="submit">Cadastrar</button>
+            </Container>
+        </Modal>
+    )
+}
